fe69b235b49d9f720578fad033164fdf57cc6649
Mariyam <mariyamakanbi12@gmail.com>
2026-03-01 17:24:07 +0100

    added new data

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿//My name is Maryam Akanbi
+﻿string name = "Maryam";
+
+int age = 40;
+
+double height = 15.8;
+
+bool isStudent = true;
+
+//////////////////////////////////////////
+
+
+//Using string Interpolation
+Console.WriteLine($"my name is {name}, i am {age} years old. My height is {height}. Are you a student: {isStudent}");
+
+//Using string concatenation
+Console.WriteLine("my name is " + name + ", i am " + age + "years old. My height is " + height + ".Are you a student:" +
+                  isStudent);
+
