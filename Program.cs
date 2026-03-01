d20ae3bab00d2a9e0a06602c52b6b7175e310d87
Mariyam <mariyamakanbi12@gmail.com>
2026-03-01 09:10:03 +0100

    My first console App

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Console.WriteLine("Hello world");
+//Console.WriteLine("My name is Maryam");
+
+//Variable Declaration(int , double , string , bool)
+
+int age = 30;
+
+string name = "Maryam";
+
+double height = 20.7;
+
+bool isMarried = false;
+
+////////////////////////////////////
+
+//Console.WriteLine(name + " " + height); // string concatination
+
+//Console.WriteLine($"{name} {height}");
+
+//FOR STRING INTERCOLATION
+Console.WriteLine($"My name is {name}, i am {age} years old. My height is height. Are you a married: {isMarried}");
+
+Console.WriteLine("my name is" + name + ", i am" + age + " years old. My height is" + height + "are you married: + isMarried");
+
+
+
