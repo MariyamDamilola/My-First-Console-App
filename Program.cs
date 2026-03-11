dd7b4dcc88208e974a1e8625b0b9aa23b21d3f27
Mariyam <mariyamakanbi12@gmail.com>
2026-03-11 18:04:22 +0100

    MARCH8 Assignment

--- Program.cs
+++ Program.cs
@@ -1,97 +1,115 @@
-﻿//Console.WriteLine("Hello world");
-//Console.WriteLine("My name is Maryam");
+﻿/*Challenge 1
+bool paidFees = true;
+bool hasIDcard = true;
+bool hasDoneMatric = false;
+bool isCitizen = false;
 
-//Variable Declaration(int , double , string , bool)
+if ((paidFees && hasIDcard) || (hasDoneMatric || isCitizen))
+{
+    Console.WriteLine("Can take the final exam");
+}
+else
+{
+    Console.WriteLine("Cannot take the final exam");
+}
 
-int age = 30;
+Explanation: The paidfee and hasidcard is a true statement, has donematric or iscitizen combined together is a false statement.
+So, combining the true or false statement, the answer is true. This means that the student must have paid the school fees and have 
+IDcard or hasdonematric or is a citizen to be able to take the final exam. Since, the statement is true, the student can take the final exam.#2#
 
-string name = "Maryam";
+//Challenge 2
+bool paidSfees = false;
+bool hasidcard = true;
+bool hasdoneregistration = true;
+bool iscitizen = false;
 
-double height = 20.7;
+if ((hasdoneregistration && paidSfees) || (iscitizen && hasidcard))
+{
+    Console.WriteLine("Can access the portal");
+}
+else
+{
+    Console.WriteLine("Cannot access the portal");
+}*/
 
-bool isMarried = false;
-
-////////////////////////////////////
-
-//Console.WriteLine(name + " " + height); // string concatenation
-
-//Console.WriteLine($"{name} {height}");
-
-//FOR STRING INTERCOLATION
-//Console.WriteLine($"My name is {name}, i am {age} years old. My height is height. Are you a married: {isMarried}");
-
-//Console.WriteLine("my name is" + name + ", i am" + age + " years old. My height is" + height + "are you married: + isMarried");
-
-
-//MARCH 7TH 2026
-decimal money = 1000746.6083683m;
-
-float temperature = 30.5f;
-gg
-char letter = 'f';
-
-
-
-//int: memory = 4byte, whole number e. g 10, 20, 30
-//float: memory = 4byte, decimal number (less precision) e. g 10.5f, 20.5f
-//double: memory = 8byte, decimal numbers e. g 10.6786, 67.967
-//long: memory = 8byte, very large numbers e. g 900000000000000000000000
-//decimal: memory = 16byte, very precise values(money), e. g bank acct balance.
-
-g
-// max : 2,147484647
-// min : -2147484647
-
-//Subject: operators
-
-// We have four operators: =, -, *, /
-
-int a = 45;
-int b = 40;
-int addedResult = a + b;
-int subResult = a - b;
-int multiplyResult = a * b;
-double divideResult = (double)a / b;
-
-//Console.WriteLine($"Adding a and b values = {addedResult}");
-//Console.WriteLine($"Subtracting a and b values = {subResult}");
-//Console.WriteLine($"Multiplying a and b values = {multiplyResult}");
-//Console.WriteLine($"Dividing a and b values = {divideResult}");
+/*Explanation: hasdoneregistration and paidfees is a false statement because one of the statement is false, in "and" statement both must be true.
+ Iscitizen is false and hasidcard is true which is still a false statement because in "and" statement both must be true. 
+ Combining two false OR statement is false. This means, student cannot access the portal #2#
+ */
 
 
-int pop1 = 200000;
-long pop2 = 9000000000;
-long popresult = pop1 + pop2;
-
-//Console.WriteLine($"Population total = {popresult}");
-
-//Note there is no precision loss
-
-//int jumahbalance = 100;
-//double sweetprice = 5.75;
-
-//double jumahamountleft = jumahbalance + sweetprice;
-//Console.WriteLine($"Jumah Total = {jumahbalanceleft}"); // no precision loss
-
-float value1 = 5.234534635634f;
-decimal value2 = 2.5m;
-
-//decimal valueResult = (decimal)value1 + value2; //precision loss
-//float valueResult = value1 + (float)value2; //no precision loss
+//Challenge 3
+/*int score = 45;*/
+/*int score = 55;*/
+/*int score = 65;*/
+/*int score = 75;*/
 
 
-//Easy Rules
-//int + long = safe
-// int + double = safe
-//float + double = some precision loss
-//double + decimal = conversion need for any variable type we want
-//float + decimal = conversion need for any variable type we want
-//decimal = best for money
+/*
+if (score >= 80)
+{
+    Console.WriteLine("A");
+}    
+else if (score >= 60)
+{
+    Console.WriteLine("B");
+}
+else if (score >= 50)
+{
+    Console.WriteLine("C");
+}
+else
+{
+    Console.WriteLine("Fail");
+}
+
+Explanation: Score 45 is below 50 which means it is fail, 
+score 55 is above 50 which means it is Grade C, 
+score 65 is above 60 which is Grade B and 
+score 75 is above 60 but below 80 which is still Grade B.*/
 
 
-//Example
-int ab = 10;
-int bc = 5;
 
-int remainder = ab % bc;
-Console.WriteLine($" ");
+
+
+/*Explanation: 
+
+// Challenge 4
+bool paidfees = true;
+bool hasIDCard = true;
+bool isStaff = false;
+bool bookReserved = false;
+
+if (paidfees && hasIDCard || isStaff && bookReserved)
+{
+    Console.WriteLine("Can borrow a book");
+}
+else
+{
+    Console.WriteLine("Cannot borrow a book");
+}
+
+/*Explanation: hasdoneregistration and paidfees is a false statement because one of the statement is false, in "and" statement both must be true.
+ Iscitizen is false and hasidcard is true which is still a false statement because in "and" statement both must be true.
+ Combining two false OR statement is false. This means, student cannot access the portal #1#
+
+
+//Challenge 5
+/*bool paidSchoolFees = true;
+bool hasIDCard = true;
+bool hasDMAtric = false;
+bool hasDoneRegistration = false;
+bool isCCitizen = false;
+
+if (paidSchoolFees && hasIDCard && (hasDMAtric || hasDoneRegistration) || isCCitizen && hasIDCard)
+{
+    Console.WriteLine("Student verified");
+}
+else
+{
+    Console.WriteLine("Student not verified");#1#
+}
+
+/*Explanation: combining paidschoolfees and hasidcard is a true statement, hasdonematric or hasdoneregistration
+ is a false statement, iscitizen and hasidcard is a false statement. The combination of true and false is false, 
+ combining false or false is also a false statement. This means that the student is not verified #1#*/
